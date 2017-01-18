3076acba434f1158a0d9fee4a94838ebf9ff2477
Marco Capuccini <marco.capuccini@farmbio.uu.se>
2017-01-18 16:01:04 +0100

    include receptor in the container

--- examples/container_usage.sh
+++ examples/container_usage.sh
@@ -1,19 +1,19 @@
 docker run \
   -v $PWD/data/:/data mcapuccini/oe-docking \
-  fred -receptor data/hiv1_protease.oeb \
+  fred -receptor /var/openeye/hiv1_protease.oeb \
   -dbase data/molecules_1.sdf \
   -docked_molecule_file data/dock_1.sdf \
   -hitlist_size 3
 
 docker run \
   -v $PWD/data/:/data mcapuccini/oe-docking \
-  fred -receptor data/hiv1_protease.oeb \
+  fred -receptor /var/openeye/hiv1_protease.oeb \
   -dbase data/molecules_2.sdf \
   -docked_molecule_file data/dock_2.sdf \
   -hitlist_size 3
 
 docker run -v $PWD/data/:/data mcapuccini/oe-docking \
-  scorepose -receptor data/hiv1_protease.oeb \
+  scorepose -receptor /var/openeye/hiv1_protease.oeb \
   -dbase data/dock_*.sdf \
   -hitlist_size 3 \
   -out data/global_top3.sdf
